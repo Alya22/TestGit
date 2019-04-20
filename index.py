54caf034fc74ed3c1c31c0c74bfbcdb48ede2bd0
unknown <Aleksandra Marushchak>
2019-04-20 22:33:04 +0300

    first

---
+++ index.py
@@ -0,0 +1,21 @@
+'''работа с GIT'''
+while True:
+    try:
+        import numpy as np; import random
+        n = 7
+        A = np.zeros(n,dtype=np.int_)
+        for i in range(n):
+            A[i] = random.randint(0,20)
+        print("Исходный массив",A)
+        print("Резельтат:",A*2)
+        print("\nВы хотите продолжить работу?")
+        d = input('\nЕсли да введите 1,если нет введите любой символ: ')
+    except (ValueError, TypeError, IndexError):
+        print("Вы ввели неверное значение!Попробуйте еще раз!")
+    else:
+        if d == '1':
+            print("Вы начали выполнение программы сначала")
+            continue
+        else:
+            print("Программа завершена.")
+        break
